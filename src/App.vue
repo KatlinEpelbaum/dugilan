8acaf7388ac46b7754c4af1977799e037cf0853c
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-10-17 12:57:26 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,102 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <header class="flex justify-between items-center px-32 py-4">
+    <div class="flex">
+      <img src="./img/Logo.svg" alt="">
+        <button class="px-6"> Browse</button>
+        <button class="px-4">All-Exclusive</button>
+        <button class="px-4">Docs</button>
+    </div>
+    <div class="flex">
+      <div class="flex p-4">
+        <input type="text" name="search">
+        <button ><img src="./img/search.svg" alt=""></button>
+      </div>
+      <img class="px-4" src="./img/heart.svg" alt="">
+      <img class="px-4 pr-6" src="./img/shopping-bag.svg" alt="">
+      <button class="bg-blue-500 text-white  py-2 px-6 rounded">Account</button>
+    </div>
+  </header>
+  <section class="flex justify-between items-center p-4">
+  <div class="flex flex-col pl-28 flex-1">
+    <p class="bg-blue-100 rounded-lg text-blue-400 w-32 text-center">Christmas sale</p>
+    <h1 class="text-4xl font-bold pt-4 pb-6">Woocommerce <br> Figma UI Kit & <br> Template</h1>
+    <div class="pb-6 flex">
+      <input class="p-4 pr-20 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 flex-1" type="text" placeholder="e.g Responsive header">
+      <button class="h-full bg-blue-500 text-white py-4 px-8 rounded-r-md border border-gray-300">Search</button>
+    </div>
+    <p class="text-gray-600">From multipurpose themes to niche templates, <br>you’ll always find something that catches your eye.</p>
+  </div>
+  <div class="flex-col flex justify-center">
+    <img class="max-w-full h-auto" src="./img/mockup2.png" alt="">
+    <p class="text-blue-500 text-lg font-bold text-start pt-6">Some of our 500 clients already using Dugilan</p>
+  </div>
+</section>
+
+  <section class="px-28 pb-20">
+    <div class="flex justify-between items-center">
+      <img src="./img/acer.png" alt="">
+      <img src="./img/facebook.png" alt="">
+      <img src="./img/linkedin.png" alt="">
+      <img src="./img/forbes.png" alt="">
+      <img src="./img/dell.png" alt="">
+    </div>
+  </section>
+<section class="bg-blue-100 flex justify-between ">
+  <div class="flex flex-col pl-32 pt-36">
+    <h1 class="text-4xl font-bold pt-4 pb-6 text-blue-500">We build apps and <br> websites</h1>
+    <p class="text-gray-700 pb-4 w-max">From multipurpose themes to niche templates, <br> you’ll always find something that catches your eye.</p>
+    <div class="flex space-x-4">
+      <button class="bg-blue-500 text-white py-4 px-6 rounded">Contact Us</button>
+      <button class="px-6 font-bold text-gray-600">or see our portfolio</button>
+    </div>
+  </div>
+  <div class="flex">
+    <img class="max-w-full h-full" src="./img/Mask Group.png" alt="">
+  </div>
+</section>
+
+  <footer>
+    <div class=" py-20 flex justify-between items-center px-20">
+      <div >
+        <h3 class="font-bold pb-4">Envato Market</h3>
+        <p class="pb-4">Terms</p>
+        <p class="pb-4">Licenses</p>
+        <p class="pb-4">Market API</p>
+        <p>Become an affiliate</p>
+      </div>
+
+      <div>
+        <h3 class="font-bold pb-4">Help</h3>
+        <p class="pb-4">Themes and Templates</p>
+        <p class="pb-4">Authors</p>
+        <p class="pb-4">Help Center</p>
+        <p>Become an affiliate</p>
+      </div>
+
+      <div>
+        <h3 class="font-bold pb-4">Our Community</h3>
+        <p class="pb-4">Community</p>
+        <p class="pb-4">Blog</p>
+        <p class="pb-4">Forums</p>
+        <p class="pb-4">Meetups</p>
+      </div>
+
+      <div>
+        <h3 class="font-bold pb-4">Contact</h3>
+        <p class="pb-4">Sabri Hakuli</p>
+        <p class="pb-4">sabrihakuli@outlook.com</p>
+        <p>+383 44 201 874</p>
+      </div>
+    </div>
+    <div class="border-2 flex justify-between items-center px-32 py-4">
+      <p class="mr-4 text-gray-600">© 2022 DugiLanding All rights reserved.</p>
+      <div class="flex space-x-2">
+        <img class="h-auto" src="./img/Frame.png" alt="">
+        <img class="h-auto" src="./img/Frame (1).png" alt="">
+        <img class="h-auto" src="./img/Frame (2).png" alt="">
+        <img class="h-auto" src="./img/Frame (3).png" alt="">
+        <img class="h-auto" src="./img/Frame (4).png" alt="">
+      </div>
+    </div>
+  </footer>
 </template>
